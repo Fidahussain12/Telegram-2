8c4780c0c55538f1ed085bdb961ad06913d8a707
Fidahussain12 <fidahussain26799@gmail.com>
2026-08-26 07:47:57 +0000

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from  "dotenv";
 import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import cookieParser from 'cookie-parser';
 import {connectDB} from './lib/db.js';
 
 import { ENV } from './lib/env.js';
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
