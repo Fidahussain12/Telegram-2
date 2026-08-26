f41b25407ba19261d11ec60fdcb5ac446661f84c
Fidahussain12 <fidahussain26799@gmail.com>
2026-08-26 06:15:02 +0000

    authentication done

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId,res) => {
     const {JWT_SECRET } = ENV;
     if(!JWT_SECRET) {
         throw new Error("JWT_SECRET is not configured");
     }
-    const token = jwt.sign({ userId },ENV.JWT_SECRET,{
+    const token = jwt.sign({ userId },process.env.JWT_SECRET,{
         expiresIn: "7d",
     });
 
@@ -13,7 +14,7 @@ export const generateToken = (userId,res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true, // prevent XSS attacks: cross-site scripting
         sameSite: "strict", // prevent CSRF attacks: cross-site request forgery
-        secure: ENV.NODE_ENV === "development" ? false : true, // set to true in production
+        secure: process.env.NODE_ENV === "development" ? false : true, // set to true in production
     });
 
     return token;
