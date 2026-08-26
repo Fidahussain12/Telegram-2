a6fcb604bd6f4ba9a19e26deb919ee9a5076348e
Fidahussain12 <fidahussain26799@gmail.com>
2026-08-26 05:18:11 +0000

    welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,11 @@
 import {generateToken} from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import {sendWelcomeEmail} from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
+
 
   try {
     if (!fullName || !email || !password) {
@@ -39,8 +42,10 @@ export const signup = async (req, res) => {
         })
 
         if(newUser){
-            generateToken(newUser._id, res)
-            await newUser.save();
+        
+
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
 
             res.status(201).json({
                 _id: newUser._id,
@@ -49,7 +54,12 @@ export const signup = async (req, res) => {
                 profilePic: newUser.profilePic,
             });
 
-            //todo: send verification email to user
+
+            try {
+              await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+              console.error("Error sending welcome email:", error);
+            }
         }
         else{
             res.status(400).json({message: "Invalid User Data"})
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId,res) => {
-    const token = jwt.sign({userId:userId},process.env.JWT_SECRET,{
+    const {JWT_SECRET } = ENV;
+    if(!JWT_SECRET) {
+        throw new Error("JWT_SECRET is not configured");
+    }
+    const token = jwt.sign({ userId },ENV.JWT_SECRET,{
         expiresIn: "7d",
     });
 
@@ -9,7 +13,7 @@ export const generateToken = (userId,res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true, // prevent XSS attacks: cross-site scripting
         sameSite: "strict", // prevent CSRF attacks: cross-site request forgery
-        secure: process.env.NODE_ENV === "development" ? false : true, // set to true in production
+        secure: ENV.NODE_ENV === "development" ? false : true, // set to true in production
     });
 
     return token;
