306144d1680f7041e59f8c0d1b40f907809f55fe
Fidahussain12 <fidahussain26799@gmail.com>
2026-08-29 00:11:49 +0000

    Message Route Added

--- backend/src/lib/arcjet.js
+++ backend/src/lib/arcjet.js
@@ -4,21 +4,18 @@ import { ENV } from "./env.js";
 const aj = arcjet({
   key: ENV.ARCJET_KEY,
   rules: [
-    // Shield protects your app from common attacks e.g. SQL injection
     shield({ mode: "LIVE" }),
-    // Create a bot detection rule
+
     detectBot({
-      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-      // Block all bots except the following
+      // Development mein block na ho is liye DRY_RUN kar dein
+      mode: process.env.NODE_ENV === "production" ? "LIVE" : "DRY_RUN",
+
       allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        // Uncomment to allow these other common bot categories
-        // See the full list at https://arcjet.com/bot-list
-        //"CATEGORY:MONITOR", // Uptime monitoring services
-        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+        "CATEGORY:SEARCH_ENGINE",
+        "POSTMAN", 
       ],
     }),
-    // Create a token bucket rate limit. Other algorithms are supported.
+
     slidingWindow({
         mode: "LIVE",
         max: 100,
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,22 @@
-import express from 'express';
-
+import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../lib/arcjet.middleware.js";
 const router = express.Router();
 
-router.get("/send", (req, res) =>{
- res.send("Send message endpoint");
-})
+//the middleware execute in order - so requests get rate-limited first, then authenticated.
+// This is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
 
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
